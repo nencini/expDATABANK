30ace93513abf93f51e4e84bbbca1632f935e790
Kiirikki Anne M <akiirikk@dx8-919-002.biocenter.helsinki.fi>
2021-07-07 10:38:46 +0300

    QualityEvaluation.py should work

--- scripts/QualityEvaluation.py
+++ scripts/QualityEvaluation.py
@@ -52,11 +52,12 @@ def prob_S_in_g(OP_exp, exp_error, OP_sim, op_sim_sd):
     return P_S
     
 # quality of simulated order parameter
-def OPquality(P_S, op_sim_err):
+def OPquality(P_S, op_sim_STEM):
     
-    quality = P_S / op_sim_err
+    quality = P_S / op_sim_STEM
+    quality_float = quality.item()
     
-    return quality
+    return quality_float
     
 ###################################################################################################
 simulations = []
@@ -95,8 +96,8 @@ for simulation in simulations:
     os.system('mkdir ../Data/QualityEvaluation/' + sub_dirs[0] + '/' + sub_dirs[1] + '/' + sub_dirs[2])
     
     os.system('mkdir ../Data/QualityEvaluation/' + sub_dirs[0] + '/' + sub_dirs[1] + '/' + sub_dirs[2] + '/' + sub_dirs[3])
+#    DATAdir = '../../NMRLipids_Data/Databank/Data/Simulations/' + str(sub_dirs[0]) + '/' + str(sub_dirs[1]) + '/' + str(sub_dirs[2]) + '/' + str(sub_dirs[3])
     DATAdir = '../Data/QualityEvaluation/' + str(sub_dirs[0]) + '/' + str(sub_dirs[1]) + '/' + str(sub_dirs[2]) + '/' + str(sub_dirs[3])
-    
     # measured values do not exist for all CH!!!!! need to take mapping name and find matching CH from simulation
     # read existing experimental values and mapping names to match with simulated CH bonds
 
@@ -105,61 +106,73 @@ for simulation in simulations:
         print(simulation.indexingPath)
         print(simulation.data.keys())
         
-        #print(OP_data_lipid)
+        OP_data_lipid = simulation.data[lipid1]
         
+        #print(OP_data_lipid)
+
+        # get readme file of the experiment
         experimentFilepath = simulation.readme['EXPERIMENT']
         READMEfilepathExperiment  = experimentFilepath + 'README.yaml'
         experiment = Experiment()
         with open(READMEfilepathExperiment) as yaml_file_exp:
             readmeExp = yaml.load(yaml_file_exp, Loader=yaml.FullLoader)
             experiment.readme = readmeExp
-            expOPdata = {}
-            for filename2 in experimentFilepath:
-                if filename2.endswith('Order_Parameters.json'):
-                   key_data2 = filename2.replace('_Order_Parameters.json','')
-                   #print(key_data2)
-                   dataPath = experimentFilepath + '/' + filename2 
-                   with open(dataPath) as json_file:
-                       expOPdata[key_data2] = json.load(json_file)
-                       json_file.close()
-            experiment.data = expOPdata
-#            experiment = Experiment(readmeExp, expOPdata, exp_subdir)
-            yaml_file_exp.close()
-        
+            print(experiment.readme)
+        yaml_file_exp.close()
+
+        lipidExpOPdata = {}
         try:
-            exp_OP_data = experiment.data[lipid1]
-        except KeyError:
+            exp_OP_filepath = simulation.readme['EXPERIMENT'] + lipid1 + '_Order_Parameters.json'
+        #print(exp_OP_filepath)
+            with open(exp_OP_filepath) as json_file:
+                lipidExpOPdata = json.load(json_file)
+            json_file.close()
+        except FileNotFoundError:
             print("Experimental order parameter data do not exist for lipid " + lipid1 + ".")
             continue
+
         exp_error = 0.02
 
         OP_qual_data = {}
         
-        for key, value in exp_OP_data.items():
-            if value[0] is not 'NaN':
-                OP_array = OP_data_lipid[key].astype(np.float) # convert elements to float because these can be string in some files
+        for key, value in lipidExpOPdata.items():
+            if lipidExpOPdata[key][0][0] is not 'NaN':
+                OP_array = [float(x) for x in OP_data_lipid[key][0]] #convert elements to float because in some files the elements are strings
+                print(OP_array)
+                print(type(OP_array))
                 OP_exp = value[0][0]
-                OP_sim = OP_array[0][0]
-           #     print(OP_data_lipid[key])
-                op_sim_sd = OP_array[0][2] #standard error of mean
-                print(key)
-                print(op_sim_STEM) 
-                op_sim_err = OP_array[0][1]
-                op_sim_sd =  OP_sd(N, op_sim_STEM)
+                OP_sim = OP_array[0]
+                op_sim_sd = OP_array[2] #standard error of mean
+                op_sim_STEM = OP_array[2] 
 
                 S_prob = prob_S_in_g(OP_exp, exp_error, OP_sim, op_sim_sd)
-            
-                OP_qual_data[key] = OPquality(S_prob, op_sim_err)
+             
+                op_quality = OPquality(S_prob, op_sim_STEM) #numpy float must be converted to float
+                print(type(op_quality))
+                OP_array.append(op_quality)
+                print(OP_array)
                 
-        outfile = DATAdir + '/' + lipid1 + '_OP_quality.json'
+                OP_qual_data[key] = OP_array
+                
+#            else:
+#                OP_qual_data[key] = value.append('nan')
+                
+#        outfile = DATAdir + '/' + lipid1 + '_OP_quality.json'
+        
+#        with open(outfile, 'w') as f:
+#            json.dump(OP_qual_data,f)
+
+#        f.close()  
+        
+        
+        # quality data should be added into the OrderParameters.json file of the simulation                  
+        outfile = DATAdir + '/' + lipid1 + '_OrderParameters.json'
         
         with open(outfile, 'w') as f:
             json.dump(OP_qual_data,f)
-
-        f.close()                    
-                
-                
-                
+        f.close()
+        
+        print(OP_qual_data)                        
                 
                 
                 
